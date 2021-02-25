de7867d01c659e12b292528e2aec18e5673f7bca
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-02-25 10:50:02 +0200

    Add

---
+++ 02.MultipleInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Eat();
+            Puppy puppy = new Puppy();
+            puppy.Weep();
+            puppy.Eat();
+            puppy.Bark();
+        }
+    }
+}
