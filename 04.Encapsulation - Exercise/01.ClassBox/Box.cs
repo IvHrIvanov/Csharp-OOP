d798e34d4acb7003c122169bd1e132c18d675f55
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-03-01 16:17:34 +0200

    Add encapsulation exercises

---
+++ 04.Encapsulation - Exercise/01.ClassBox/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length {
+            get
+            {
+                return this.length; 
+            }
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public string SurfaceArea(double length, double width, double height)
+        {
+            double sum = (2 * length * width) + (2 * length * height) + (2 * width * height);
+
+            return $"Surface Area - {sum:f2}";
+        }
+        public string LateralSurfaceArea(double length, double width, double height)
+        {
+            double sum = (2 * length * height) + (2 * width * height);
+            return $"Lateral Surface Area - {sum:f2}";
+        }
+        public string Volume(double length, double width, double height)
+        {
+            double sum = height * length * width;
+            return $"Volume - {sum:f2}";
+        }
+    }
+}
---
+++ 04.Encapsulation - Exercise/01.ClassBox/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.ClassBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine(box.SurfaceArea(length, width, height));
+                Console.WriteLine(box.LateralSurfaceArea(length, width, height));
+                Console.WriteLine(box.Volume(length, width, height));
+
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
