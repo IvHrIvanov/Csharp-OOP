6a7aa8c1c28e17c215cad04de1a19a3c3d7d1aa4
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-03-04 23:44:59 +0200

    Add and Update

---
+++ 06.InterfacesAndAbstraction-Exercises/05.BirthdayCelebrations/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Pet : Birthdates
+    {
+        public Pet(string name, string birthdates)
+        {
+            Name = name;
+            Birthdates = birthdates;
+            this.Id = Id;
+        }
+
+        public string Name { get; private set; }
+
+        public string Birthdates { get; private set; }
+
+        public string Id { get; set; }
+
+    }
+}
---
+++ 06.InterfacesAndAbstraction-Exercises/05.BirthdayCelebrations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Birthdates> identifiables = new List<Birthdates>();
+            List<IIdentifiable> robots = new List<IIdentifiable>();
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "End")
+            {
+                string currentPerson = input[0];
+                if (currentPerson == "Citizen")
+                {
+                    string name = input[1];
+                    int age = int.Parse(input[2]);
+                    string id = input[3];
+                    string bitrhdate = input[4];
+                    identifiables.Add(new Citizen(name, age, id, bitrhdate));
+
+                }
+                else if (currentPerson == "Robot")
+                {
+                    string model = input[0];
+                    string id = input[1];
+                    robots.Add(new Robot(id, model));
+                }
+                else if (currentPerson == "Pet")
+                {
+                    string birthdates = input[2];
+
+                    identifiables.Add(new Pet(currentPerson, birthdates));
+                }
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            string findYear = Console.ReadLine();
+
+            List<Birthdates> filtered = identifiables.Where(x => x.Birthdates.EndsWith(findYear)).ToList();
+           
+                foreach (var item in filtered)
+                {
+                    Console.WriteLine(item.Birthdates);
+                }
+            
+        }
+    }
+}
