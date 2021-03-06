ed6461bc579e9e182541651839413e48dcd41c3c
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-03-07 00:09:09 +0200

    Add polymorhpism - Lab

---
+++ 07.Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+   public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
