3bbb24025d7c672fb9889b56470ff519da69ba2b
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-03-21 23:45:54 +0200

    Add

---
+++ 10.UnitTest-Lab/01.AxeTest/Class1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Test
+{
+
+    public class Class1
+    {
+        private const int AxeAtack = 1;
+        private const int AxeDurability = 1;
+        private const int DummyHealth = 10;
+        private const int DummyExperience = 10;
+        private Axe axe;
+        private Dummy dummy;
+        [SetUp]
+        public void TestInit()
+        {
+            this.axe = new Axe(AxeAtack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+        }   
+        [Test]
+        public void BrokenAxeCantAttack()
+        {
+            // Arragne (made in TestInit)
+
+            // Act
+            this.axe.Attack(this.dummy);
+
+            // Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
+
---
+++ 10.UnitTest-Lab/02.DummyTest/DummyTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Test
+{
+
+    public class Class1
+    {
+
+        private const int DummyHealth = 5;
+        private const int DummyExperience = 10;
+
+        private Dummy dummy;
+        [SetUp]
+        public void TestInit()
+        {
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+        }
+
+        [Test]
+        public void DummyAreLosesHealthIfAtacked()
+        {
+            int attackPoints = 3;
+            this.dummy.TakeAttack(attackPoints);
+            Assert.That(dummy.Health, Is.EqualTo(DummyHealth - attackPoints));
+        }
+        [Test]
+        public void DeadDummyThrowsExceptionIfAtacked()
+        {
+
+            Assert.That(dummy.IsDead(), Is.EqualTo(false));
+        }
+        [Test]
+        public void DeadDummyThrowsExceptioZero()
+        {
+            dummy = new Dummy(0, DummyExperience);
+            Assert.That(dummy.IsDead(), Is.EqualTo(true));
+
+        }
+        [Test]
+        public void DeadDumyNegative()
+        {
+            dummy = new Dummy(-10, DummyExperience);
+            Assert.That(dummy.IsDead(), Is.EqualTo(true));
+        }
+        [Test]
+        public void WhenAttackDeadDummy_Throw()
+        {
+            dummy = new Dummy(-5, DummyExperience);
+            Assert.That(() =>
+            {
+                dummy.TakeAttack(3);
+            }, Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+        }
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            dummy = new Dummy(0, DummyExperience);
+            Assert.That(dummy.GiveExperience(), Is.EqualTo(DummyExperience));
+        }
+        [Test]
+        public void AliveDummyCantGiveXp()
+        {
+            dummy = new Dummy(5, 5);
+            Assert.That(() =>
+            {
+                dummy.GiveExperience();
+            },Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+            
+        }
+    }
+}
+
